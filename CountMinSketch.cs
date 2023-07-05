355195c467b159859ea33716f0c550041bb2d250
Bimka09 <90Alex09@mail.ru>
2023-07-05 12:46:39 +0300

    Добавьте файлы проекта.

---
+++ CountMinSketch.cs
@@ -0,0 +1,56 @@
+﻿using System.Text;
+
+namespace ProbabilisticAlgorithm
+{
+    internal class CountMinSketch
+    {
+        private int d;
+        private int w;
+        private int[,] table;
+        private int[] hashes;
+
+        public CountMinSketch(int d, int w)
+        {
+            this.d = d;
+            this.w = w;
+            table = new int[d, w];
+            hashes = new int[d];
+            Random random = new Random();
+            for (int i = 0; i < d; i++)
+            {
+                hashes[i] = random.Next();
+            }
+        }
+
+        public void Add(string value, int count)
+        {
+            for (int i = 0; i < d; i++)
+            {
+                uint index = Convert.ToUInt32(Hash(value, hashes[i]) % w);
+                table[i, index] += count;
+            }
+        }
+
+        public int Count(string value)
+        {
+            int minCount = int.MaxValue;
+            for (int i = 0; i < d; i++)
+            {
+                uint index = Convert.ToUInt32(Hash(value, hashes[i]) % w);
+                minCount = Math.Min(minCount, table[i, index]);
+            }
+            return minCount;
+        }
+
+        private uint Hash(string value, int hash)
+        {
+            byte[] bytes = Encoding.UTF8.GetBytes(value);
+            uint result = (uint)hash;
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                result = (result * 31 + bytes[i]) % int.MaxValue;
+            }
+            return result;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using ProbabilisticAlgorithm;
+
+CountMinSketch cms = new CountMinSketch(4, 100);
+cms.Add("apple", 5);
+cms.Add("banana", 3);
+cms.Add("cherry", 74);
+cms.Add("cherry", 70);
+cms.Add("cherry", 6);
+cms.Add("cherry", 7);
+int count = cms.Count("apple");
+Console.WriteLine(count); 
